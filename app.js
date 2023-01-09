66700d0f96e642a77205356e3e5fb9fb56703e47
SMwebdeveloper <samandarwebdeveloper@gamil.com>
2023-01-09 17:01:04 +0500

    quote function

--- app.js
+++ app.js
@@ -1,34 +1,70 @@
-const romanNumber = (num) => {
-    const romanNUm = {
-        M:1000,
-        CM:900,
-        D:500,
-        CD:400,
-        C:100,
-        XC:90,
-        L:50,
-        XL:40,
-        X:10,
-        IX:9,
-        V:5,
-        IV:4,
-        I:1,
-    }
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
-    let romen = ''
+let apiQuotes = []
 
-    for (const key in romanNUm.leng) {
-        // console.log("key ",key);
-        // console.log("value ",romanNUm[key]);
+// loader
 
-        while(num >= romanNUm[key]){
-            romen += key
-            num.length -= romanNUm[key]
-            console.log(num);
-        }
-    }
-
-    return romen
+function loading(){
+    loader.hidden = false
+    quoteContainer.hidden = true
 }
 
-romanNumber(3)
+
+// hide loading
+function complete(){
+    if (!loader.hidden) {
+        quoteContainer.hidden = false
+        loader.hidden = true
+    }
+}
+// get api
+async function getQuote(){
+    loading()
+    // const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+    const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json'
+    try{
+        const response = await fetch(apiUrl)
+        const apiQuotes = await response.json()
+        const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
+        console.log(apiQuotes);
+        // newQuote()
+        console.log(quote.text);
+
+        if (quote.author === '') {
+            authorText.innerText = 'Unknown'
+        } else {
+            authorText.innerText = quote.author
+        }
+
+        if (quote.text > 100) {
+            quoteText.classList.remove('long-quote')
+        } else {
+            quoteText.classList.add('long-quote')
+        }
+        quoteText.innerText = quote.text
+        complete()
+    } catch (error){
+        getQuote()
+      
+    }
+}
+
+
+function tweetQuote() {
+    const quote = quoteText.innerText
+    const author = authorText.innerText
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`
+    window.open(twitterUrl, '_blank')
+}
+// add event listener
+newQuoteBtn.addEventListener('click', getQuote),
+twitterBtn.addEventListener('click', tweetQuote)
+
+// on load
+getQuote()
+// loading()
